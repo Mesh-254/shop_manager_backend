b737a341ea99db6d045cb1847c4ba4a301652402
Mesh-254 <Meshack1995@gmail.com>
2026-02-09 18:40:11 +0300

    added google accounts auth

--- backend/urls.py
+++ backend/urls.py
@@ -25,6 +25,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 
     # JWT authentication endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
