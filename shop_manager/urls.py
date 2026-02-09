5d7b491636a0ac1952c166c464fc4c817a9e8724
Mesh-254 <Meshack1995@gmail.com>
2026-02-09 18:43:50 +0300

    upgraded urls

--- shop_manager/urls.py
+++ shop_manager/urls.py
@@ -12,10 +12,11 @@ router.register(r'categories', views.CategoryViewSet, basename='category')
 router.register(r'suppliers', views.SupplierViewSet, basename='supplier')
 router.register(r'products', views.ProductViewSet, basename='product')
 router.register(r'stocks', views.StockViewSet, basename='stock')
+router.register(r'stock-transactions', views.StockTransactionViewSet, basename='stock-transaction')
 router.register(r'purchases', views.PurchaseViewSet, basename='purchase')
-router.register(r'purchaseitems', views.PurchaseItemsViewSet, basename='purchaseitem')
+router.register(r'purchaseitems', views.PurchaseItemViewSet, basename='purchaseitem')
 router.register(r'sales', views.SaleViewSet, basename='sale')
-router.register(r'saleitems', views.SaleItemsViewSet, basename='saleitem')
+router.register(r'saleitems', views.SaleItemViewSet, basename='saleitem')
 
 
 urlpatterns = [
